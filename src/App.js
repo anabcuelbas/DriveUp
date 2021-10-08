ef7dfad12209e15ba9a5d408f234f6ffeac2fb7b
anabcuelbas <abcuelbas@gmail.com>
2021-10-07 22:56:04 -0300

    Testing deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esse vai ser um Hub de carro um dia.
         </p>
         <a
           className="App-link"
