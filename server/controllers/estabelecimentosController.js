b9aa320f30229aee000e7a3e89ead0f33fafec7f
anabcuelbas <abcuelbas@gmail.com>
2021-11-01 11:49:42 -0300

    Adiciona controllers para o restante das funções do primeiro ciclo

---
+++ server/controllers/estabelecimentosController.js
@@ -0,0 +1,45 @@
+require('dotenv/config')
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: '5432'
+})
+
+const getAllEstabelecimentos = async (req, res) => {
+    const query = "SELECT * FROM estabelecimento";
+    const response = await pool.query(query, (err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    });
+}
+
+const getEstabelecimentosWithServico = async (req, res) => {
+    const query = `
+        SELECT * FROM estabelecimento
+        WHERE estabelecimento.id IN (
+            SELECT estabelecimentoId 
+            FROM servico_estabelecimento
+            WHERE servicoId = ${req.body.servicoId}
+        )
+    `;
+
+    const response = await pool.query(query, (err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    });
+}
+
+module.exports = {
+    getAllEstabelecimentos,
+    getEstabelecimentosWithServico
+}
