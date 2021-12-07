94136017c6bdad0149f36dc52f1f716757198e0a
Laura <laurazsouza@yahoo.com.br>
2021-12-06 21:27:43 -0300

    feat: adiciona página de perfil nas rotas e no header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,46 +2,35 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
-  },
-  {
-    path: 'lavagem',
-    loadChildren: () =>
-      import('./lavagem/lavagem.module').then((m) => m.LavagemPageModule),
-  },
-  {
-    path: 'agendamento',
-    loadChildren: () =>
-      import('./agendamento/agendamento.module').then(
-        (m) => m.AgendamentoPageModule
-      ),
-  },
-  {
-    path: 'sobre',
-    loadChildren: () =>
-      import('./sobre-nos/sobre-nos.module').then((m) => m.SobreNosModule),
-  },
-  {
-    path: 'funcionamento',
-    loadChildren: () =>
-      import('./funcionamento/funcionamento.module').then(
-        (m) => m.FuncionamentoModule
-      ),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+	{
+		path: 'home',
+		loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+	},
+	{
+		path: 'lavagem',
+		loadChildren: () => import('./lavagem/lavagem.module').then((m) => m.LavagemPageModule),
+	},
+	{
+		path: 'agendamento',
+		loadChildren: () => import('./agendamento/agendamento.module').then((m) => m.AgendamentoPageModule),
+	},
+	{
+		path: 'sobre',
+		loadChildren: () => import('./sobre-nos/sobre-nos.module').then((m) => m.SobreNosModule),
+	},
+	{
+		path: 'perfil',
+		loadChildren: () => import('./perfil/perfil.module').then((m) => m.PerfilModule),
+	},
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
