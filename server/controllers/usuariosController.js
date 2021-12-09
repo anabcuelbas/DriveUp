755146194b6308260db1c335033ae925bdb6c493
anabcuelbas <abcuelbas@gmail.com>
2021-12-08 23:11:52 -0300

    Adiciona rotas de login, cadastro e atualização de usuario/estabelecimento

---
+++ server/controllers/usuariosController.js
@@ -0,0 +1,111 @@
+require('dotenv/config')
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: '5432'
+})
+
+const getUsuario = async (req, res) => {
+    const query = `
+        SELECT * 
+        FROM usuario
+        WHERE usuario.email IN (
+            SELECT perfil.email 
+            FROM  perfil
+            WHERE perfil.email = '${req.params.perfilEmail}' AND perfil.senha = '${req.params.perfilSenha}'
+        )
+    `;
+
+    const response = await pool.query(query, (err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    });
+}
+
+const createUsuario = async (req, res) => {
+    let email = req.body.email;
+    let senha = req.body.senha;
+    let tipo = req.body.tipo;
+
+    let telefone = req.body.telefone;
+    let nomeUsuario = req.body.nomeUsuario;
+
+    let nomeEmpresa = req.body.nomeEmpresa;
+    let endereco = req.body.endereco;
+    let hora = req.body.horaFuncionamento;
+    let dia = req.body.diasFuncionamento;
+    let imagem = req.body.img;
+
+    let query;
+
+    if (tipo == 'empresa') {
+        query = "INSERT INTO estabelecimento (Nome,  Endereco, HoraFuncionamento, DiasFuncionamento, image_url) VALUES ('" + nomeEmpresa + "', '"  + endereco + "', '" + hora + "', '" + dia + "', '" + imagem + "'); INSERT INTO perfil (email,  senha) VALUES ('" + email + "', '"  + senha + "');"
+    } else if (tipo == 'usuario') {
+        query = "INSERT INTO usuario (nome,  telefone, email) VALUES ('" + nomeUsuario + "', '"  + telefone + "', '" + email + "'); INSERT INTO perfil (email,  senha) VALUES ('" + email + "', '"  + senha + "');"
+    }
+
+    const response = await pool.query(query, async(err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    }); 
+}
+
+const updateUsuario = async (req, res) => {
+    let email = req.body.email;
+    let telefone = req.body.telefone;
+    let nome = req.body.nome;
+
+    const query = `
+        UPDATE usuario
+        SET telefone = '${telefone}', nome = '${nome}'
+        WHERE usuario.email = '${email}'
+    `;
+
+    const response = await pool.query(query, async(err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    }); 
+}
+
+const updateEstabelecimento = async (req, res) => {
+    let email = req.body.email;
+    let nome = req.body.nome;
+    let endereco = req.body.endereco;
+    let hora = req.body.horaFuncionamento;
+    let dia = req.body.diasFuncionamento;
+    let imagem = req.body.img;
+
+    const query = `
+        UPDATE estabelecimento
+        SET nome = '${nome}', endereco = '${endereco}', horaFuncionamento = '${hora}', diasFuncionamento = '${dia}', image_url = '${imagem}'
+        WHERE estabelecimento.email = '${email}'
+    `;
+
+    const response = await pool.query(query, async(err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    }); 
+}
+
+module.exports = {
+    getUsuario,
+    createUsuario,
+    updateUsuario,
+    updateEstabelecimento
+}
